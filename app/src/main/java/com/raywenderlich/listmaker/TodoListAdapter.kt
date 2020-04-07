a665fcde9aa3c7c70012468985ae255a382d62e5
Stefan Novak <stefannovak96@gmail.com>
2020-04-07 17:25:18 +0100

    List Created
    Signed-off-by: Stefan Novak <stefannovak96@gmail.com>

--- app/src/main/java/com/raywenderlich/listmaker/TodoListAdapter.kt
+++ app/src/main/java/com/raywenderlich/listmaker/TodoListAdapter.kt
@@ -1,5 +1,6 @@
 package com.raywenderlich.listmaker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
@@ -8,15 +9,19 @@ public class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
     private val todoLists = arrayOf("Android Development", "House Work", "Errands")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.todo_list_view_holder, parent, false)
+        return TodoListViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         return todoLists.size
     }
 
     override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        holder.listPositionTextView.text = (position + 1).toString()
+        holder.listTitleTextView.text = todoLists[position]
+
     }
 }
